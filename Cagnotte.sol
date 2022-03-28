e019939286114c8257cb9936483f0b1b564f3407
Alexis Jbrt <86856769+ajobart@users.noreply.github.com>
2022-03-28 18:50:56 +0200

    Update Cagnotte.sol

--- Cagnotte.sol
+++ Cagnotte.sol
@@ -1,175 +1,139 @@
 pragma solidity ^0.8.7;
 
-contract TestCagnotte {
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+
+contract TestCagnotte is ERC721Enumerable, ERC721Burnable, Ownable {
+
+    // Résolution d'un conflit 
+    function _beforeTokenTransfer(address from, address to, uint256 tokenId) internal override(ERC721, ERC721Enumerable) {
+        super._beforeTokenTransfer(from, to, tokenId);
+    }
+
+    function supportsInterface(bytes4 interfaceId) public view override(ERC721, ERC721Enumerable) returns (bool) {
+        return super.supportsInterface(interfaceId);
+    }
+
 
     address contractOwner;
 
     struct Participant {
-        address addressParticipant;
-        uint nbrTotalDonsDuDonnateur;
-        uint balanceDonnations;
+        address participantAddress;
+        uint amount;
     }
-
+    
     mapping(address => Participant) participants;
 
     struct Cagnotte {
-        address owner;
+        bool exist;
+        address cagnotteOwner;
         string name;
         uint balance;
-        uint nbrDeDons;
         uint nbrParticipants;
         uint goal;
-        bool goalAchieved;
         bool isClosed;
         address[] allParticipants;
+        address[] participants;
     }
+    
 
     uint cagnotteId = 0;
     
     mapping(uint => Cagnotte) cagnottes;
     
 
-    constructor() {
+    constructor() ERC721("Cagnotte NFT", "Cagnotte") {
         contractOwner = msg.sender;
+        cagnotteId++;
     }
 
     // Fonction pour créer une cagnotte
-    function addCagnotte(string memory _name, uint _goal) public {
-        // On vérifie si la cagnotte existe déjà 
-        require(keccak256(abi.encodePacked(_name)) != keccak256(abi.encodePacked(cagnottes[cagnotteId].name)), 'This cagnotte already exist');
-        // Si la cagnotte n'existe pas alors on l'ajoute
-        cagnotteId ++;
-        // La personne ajoutant une cagnotte est le propriétaire de celle ci
-        cagnottes[cagnotteId].owner = msg.sender;
+    function mintCagnotte(string memory _name, uint _goal) external {
+        require(_goal > 0, 'The goal can\'t be negative');
+        cagnottes[cagnotteId].exist = true;
+        cagnottes[cagnotteId].cagnotteOwner = msg.sender;
         cagnottes[cagnotteId].name = _name;
         cagnottes[cagnotteId].balance = 0;
         cagnottes[cagnotteId].nbrParticipants = 0;
-        cagnottes[cagnotteId].nbrDeDons = 0;
-        require(_goal > 0, 'The goal can\'t be negative');
         cagnottes[cagnotteId].goal = _goal;
-        cagnottes[cagnotteId].goalAchieved = false;
-        cagnottes[cagnotteId].isClosed = false;        
+        cagnottes[cagnotteId].isClosed = false;  
+        _safeMint(msg.sender, cagnotteId);
     }
 
-    //Fonction pour voir le nom d'une cagnotte
-    function getName(uint _cagnotteId) public view returns(string memory) {
-        return cagnottes[_cagnotteId].name;
+    // Fonction pour avoir les details d'une cagnotte
+    function getDetails(uint _cagnotteId) public view returns(string memory, uint, uint, uint, bool, address[] memory) {
+        return (cagnottes[_cagnotteId].name, cagnottes[_cagnotteId].balance, cagnottes[_cagnotteId].nbrParticipants, cagnottes[_cagnotteId].goal, cagnottes[_cagnotteId].isClosed, cagnottes[_cagnotteId].allParticipants);
     }
-
-    //Fonction pour voir l'owner d'une cagnotte
-    function getOwner(uint _cagnotteId) public view returns(address) {
-        return cagnottes[_cagnotteId].owner;
-    }
-
-
-    //Fonction pour voir le montant 'goal' d'une cagnotte
-    function getGoal(uint _cagnotteId) public view returns(uint) {
-        return cagnottes[_cagnotteId].goal;
-    }
-
-    //Fonction pour voir si le goal d'une cagnotte est atteint
-    function getStatutGoal(uint _cagnotteId) public view returns(bool) {
-        return cagnottes[_cagnotteId].goalAchieved;
-    }
-
-    //Fonction pour changer le goal d'une cagnotte
-    function setGoal(uint _cagnotteId, uint _newGoal) public {
-        require(msg.sender == cagnottes[_cagnotteId].owner, "Your not the owner of this cagnotte");
-        cagnottes[_cagnotteId].goal = _newGoal;
-    }
-
-    //Fonction pour voir la balance d'une cagnotte
-    function getBalance(uint _cagnotteId) public view returns(uint) {
-        return cagnottes[_cagnotteId].balance;
-    }
-
-    //Fonction pour voir le nombre de dons d'une cagnotte
-    function getNbrDeDons(uint _cagnotteId) public view returns(uint) {
-        return cagnottes[_cagnotteId].nbrDeDons;
-    }
-
-    //Fonction pour voir si une cagnotte est fermé
-    function isClosed(uint _cagnotteId) public view returns(bool) {
-        return cagnottes[_cagnotteId].isClosed;
-    }
-
+    
     //Fonction Payable pour qu'un utilisateur fasse un don du montant qu'il souhaite à une cagnotte
     function payCagnotte(uint _cagnotteId) payable public {
         //On vérifie que la cagnotte existe
-        require(keccak256(abi.encodePacked(cagnottes[_cagnotteId].name)) == keccak256(abi.encodePacked(cagnottes[cagnotteId].name)), 'This cagnotte no exist');
+        //require(keccak256(abi.encodePacked(cagnottes[_cagnotteId].name)) == keccak256(abi.encodePacked(cagnottes[cagnotteId].name)), 'This cagnotte no exist');
+        require(cagnottes[_cagnotteId].exist == true, "Cagnotte does not exist");
         //On vérifie que la cagnotte n'est pas terminé
         require(cagnottes[_cagnotteId].isClosed == false, 'La cagnotte est termine');
         //Le participants doit faire un don d'une valeur supérieur à 0.
         require(msg.value > 0, 'Le montant est insuffisant');
-        cagnottes[cagnotteId].balance += msg.value;
-        cagnottes[cagnotteId].nbrDeDons++;
-        participants[msg.sender].addressParticipant = msg.sender;
-        participants[msg.sender].nbrTotalDonsDuDonnateur++;
-        participants[msg.sender].balanceDonnations = participants[msg.sender].balanceDonnations + msg.value;
+        cagnottes[_cagnotteId].balance += msg.value;
         // On ajoute le donateur aux participants
         //On ne stock pas plusieurs fois l'address d'un participant si il fait plusieurs fois des dons sur une même cagnotte
-        
-        if (cagnottes[_cagnotteId].allParticipants.length > 0) {
-            for(uint i = 0; i < cagnottes[_cagnotteId].allParticipants.length; i++) {
-                if (cagnottes[_cagnotteId].allParticipants[i] != msg.sender) {
-                    cagnottes[_cagnotteId].allParticipants.push(msg.sender);
-                } else {
-                   '';
-                }
-            }
-        } else {
-            cagnottes[_cagnotteId].allParticipants.push(msg.sender);
-        }
-
-        /* TEST
-        for(uint i = 0; i < cagnottes[_cagnotteId].allParticipants.length; i++) {
-            if (cagnottes[_cagnotteId].allParticipants.length == 0) {
-                cagnottes[_cagnotteId].allParticipants.push(msg.sender);
-            }
-            else if (cagnottes[_cagnotteId].allParticipants[i] != msg.sender) {
-                cagnottes[_cagnotteId].allParticipants.push(msg.sender);
-            } 
-            else {
-                '';
-            }
-        }
-        */
-
-        //On verifie si le but de la cagnotte est atteint
-        if (cagnottes[cagnotteId].balance >= cagnottes[cagnotteId].goal) {
-            cagnottes[cagnotteId].goalAchieved = true;
-        } 
-        else {
-            cagnottes[cagnotteId].goalAchieved = false;
-        }
+        cagnottes[_cagnotteId].participants.push(msg.sender);
     }
 
+    //Pour burn un NFT
+    function burn(uint _cagnotteId) public virtual override {
+        _burn(_cagnotteId);
+        cagnottes[_cagnotteId].exist = false;
+    }
+
+    //Pour retirer l'argent
+    function withdraw(uint _cagnotteId) public payable {    
+        (bool success, ) = payable(cagnottes[_cagnotteId].cagnotteOwner).call{value: address(this).balance}("Your not the owner of this cagnotte");
+        require(success);
+        cagnottes[_cagnotteId].balance = 0;
+        cagnottes[cagnotteId].isClosed = true;
+    }
+
+
+    //Fonction pour changer le goal d'une cagnotte
+    function setGoal(uint _cagnotteId, uint _newGoal) public {
+        cagnottes[_cagnotteId].goal = _newGoal;
+    }
+
+
+    //Fonction pour voir le nom d'une cagnotte
+    function getName(uint _cagnotteId) internal view returns(string memory) {
+        return cagnottes[_cagnotteId].name;
+    }
+
+
+    //Fonction pour voir le montant 'goal' d'une cagnotte
+    function getGoal(uint _cagnotteId) internal view returns(uint) {
+        return cagnottes[_cagnotteId].goal;
+    }
+
+    //Fonction pour voir la balance d'une cagnotte
+    function getBalance(uint _cagnotteId) internal view returns(uint) {
+        return cagnottes[_cagnotteId].balance;
+    }
+
+    //Fonction pour voir si une cagnotte est fermé
+    function isClosed(uint _cagnotteId) internal view returns(bool) {
+        return cagnottes[_cagnotteId].isClosed;
+    }
+
+
     //Verifier la longueur de l'array Allparticipant
-    function getNbrParticipants(uint _cagnotteId) public view returns(uint) {
+    function getNbrParticipants(uint _cagnotteId) internal view returns(uint) {
         return cagnottes[_cagnotteId].allParticipants.length;
     }
 
     //Fonction pour voir les participants d'une cagnotte
-    function getParticipants(uint _cagnotteId) public view returns(address[] memory) {
+    function getParticipants(uint _cagnotteId) internal view returns(address[] memory) {
         return cagnottes[_cagnotteId].allParticipants;
-    }
-
-    //Fonction pour connaitre le nombre de donnation d'un participant
-   function getNbrDonnationOfParticipant(address _addressParticipant) public view returns(uint) {
-        return participants[_addressParticipant].nbrTotalDonsDuDonnateur;
-   }
-
-   //Fonction pour connaitre le montant de donnation d'un participant
-   function getBalanceDonnationsOfParticipant(address _addressParticipant) public view returns(uint) {
-       return participants[_addressParticipant].balanceDonnations;
-   }
-
-    //Pour retirer l'argent
-    function withdraw(uint _cagnotteId) public payable {    
-        require(msg.sender == cagnottes[_cagnotteId].owner);
-        (bool success, ) = payable(cagnottes[_cagnotteId].owner).call{value: address(this).balance}("Your not the owner of this cagnotte");
-        require(success);
-        cagnottes[_cagnotteId].balance = 0;
-        cagnottes[cagnotteId].isClosed = true;
     }
 }
