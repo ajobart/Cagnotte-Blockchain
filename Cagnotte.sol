4284edb8574929133e2a5d436a69b0ccf6e5b01d
Alexis Jbrt <86856769+ajobart@users.noreply.github.com>
2022-03-30 00:16:06 +0200

    Update Cagnotte.sol

--- Cagnotte.sol
+++ Cagnotte.sol
@@ -9,7 +9,7 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract TestCagnotte is ERC721Enumerable, ERC721Burnable, Ownable {
 
-    // Résolution d'un conflit 
+    // Résolution d'un conflit
     function _beforeTokenTransfer(address from, address to, uint256 tokenId) internal override(ERC721, ERC721Enumerable) {
         super._beforeTokenTransfer(from, to, tokenId);
     }
@@ -18,14 +18,11 @@ contract TestCagnotte is ERC721Enumerable, ERC721Burnable, Ownable {
         return super.supportsInterface(interfaceId);
     }
 
-
-    address contractOwner;
-
     struct Participant {
         address participantAddress;
         uint amount;
     }
-    
+
     mapping(address => Participant) participants;
 
     //Pour increment l'id des NFTs(cagnotte)
@@ -35,9 +32,6 @@ contract TestCagnotte is ERC721Enumerable, ERC721Burnable, Ownable {
     Counters.Counter private _nftIdCounter;
 
     struct Cagnotte {
-        bool exist;
-        address owner;
-        address cagnotteOwner;
         string name;
         uint balance;
         uint nbrParticipants;
@@ -45,45 +39,41 @@ contract TestCagnotte is ERC721Enumerable, ERC721Burnable, Ownable {
         bool isClosed;
         Participant[] _Participants;
     }
-        
+
     mapping(uint => Cagnotte) cagnottes;
-    
 
     constructor() ERC721("Cagnotte NFT", "Cagnotte") {
-        contractOwner = msg.sender;
+        transferOwnership(msg.sender);
         _nftIdCounter.increment();
     }
 
     // Fonction pour créer une cagnotte
     function mintCagnotte(string memory _name, uint _goal) external {
         require(_goal > 0, 'The goal can\'t be negative');
-        cagnottes[_nftIdCounter.current()].exist = true;
-        cagnottes[_nftIdCounter.current()].owner = msg.sender;
-        cagnottes[_nftIdCounter.current()].cagnotteOwner = msg.sender;
         cagnottes[_nftIdCounter.current()].name = _name;
         cagnottes[_nftIdCounter.current()].balance = 0;
         cagnottes[_nftIdCounter.current()].nbrParticipants = 0;
         cagnottes[_nftIdCounter.current()].goal = _goal;
-        cagnottes[_nftIdCounter.current()].isClosed = false;  
+        cagnottes[_nftIdCounter.current()].isClosed = false;
         _safeMint(msg.sender, _nftIdCounter.current());
         _nftIdCounter.increment();
     }
 
     //Pour que seul le propriétaire d'une cagnotte puisse faire certaines actions
-	modifier isOwner(uint _nftId) {
-		require(msg.sender == cagnottes[_nftId].owner, "Not the owner");
-		_; 
-	}
+    modifier isOwner(uint _nftId) {
+        require(msg.sender == ownerOf(_nftId), "Not the owner");
+        _;
+    }
 
     // Fonction pour avoir les details d'une cagnotte
-    function getDetails(uint _nftId) public view returns(address, string memory, uint, uint, uint, bool, Participant[] memory) {
-        return (cagnottes[_nftId].owner, cagnottes[_nftId].name, cagnottes[_nftId].balance, cagnottes[_nftId].nbrParticipants, cagnottes[_nftId].goal, cagnottes[_nftId].isClosed, cagnottes[_nftId]._Participants);
+    function getDetails(uint _nftId) public view returns(string memory, uint, uint, uint, bool, Participant[] memory) {
+        return (cagnottes[_nftId].name, cagnottes[_nftId].balance, cagnottes[_nftId].nbrParticipants, cagnottes[_nftId].goal, cagnottes[_nftId].isClosed, cagnottes[_nftId]._Participants);
     }
-    
+
     //Fonction Payable pour qu'un utilisateur fasse un don du montant qu'il souhaite à une cagnotte
     function payCagnotte(uint _nftId) payable public {
         //On vérifie que la cagnotte existe
-        require(cagnottes[_nftId].exist == true, "Cagnotte does not exist");
+        require(_exists(_nftId) == true);
         //On vérifie que la cagnotte n'est pas terminé
         require(cagnottes[_nftId].isClosed == false, 'La cagnotte est termine');
         //Le participants doit faire un don d'une valeur supérieur à 0.
@@ -94,10 +84,9 @@ contract TestCagnotte is ERC721Enumerable, ERC721Burnable, Ownable {
         cagnottes[_nftId]._Participants.push(thisParticipant);
     }
 
-
     //Pour retirer l'argent
-    function withdraw(uint _nftId) public payable isOwner(_nftId) {  
-        (bool success, ) = payable(cagnottes[_nftId].cagnotteOwner).call{value: address(this).balance}("Your not the owner of this cagnotte");
+    function withdraw(uint _nftId) public payable {
+        (bool success, ) = payable(ownerOf(_nftId)).call{value: cagnottes[_nftId].balance}("Your not the owner of this cagnotte");
         require(success);
         cagnottes[_nftId].balance = 0;
         cagnottes[_nftId].isClosed = true;
@@ -105,18 +94,15 @@ contract TestCagnotte is ERC721Enumerable, ERC721Burnable, Ownable {
         burn(tokenId);
     }
 
-
     //Fonction pour changer le goal d'une cagnotte
     function setGoal(uint _nftId, uint _newGoal) public isOwner(_nftId) {
         cagnottes[_nftId].goal = _newGoal;
     }
 
-
     //Fonction pour voir le nom d'une cagnotte
     function getName(uint _nftId) internal view returns(string memory) {
         return cagnottes[_nftId].name;
     }
-
 
     //Fonction pour voir le montant 'goal' d'une cagnotte
     function getGoal(uint _nftId) internal view returns(uint) {
@@ -132,7 +118,6 @@ contract TestCagnotte is ERC721Enumerable, ERC721Burnable, Ownable {
     function isClosed(uint _nftId) internal view returns(bool) {
         return cagnottes[_nftId].isClosed;
     }
-
 
     //Verifier la longueur de l'array Allparticipant
     function getNbrParticipants(uint _nftId) internal view returns(uint) {
